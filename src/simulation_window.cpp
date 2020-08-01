ef351f924872ff3508edb54a102eb9ff851c72c3
Ilya Busov <busov.ilyaa@gmail.com>
2020-08-01 09:50:33 +0300

    added ability to push body by force

--- src/simulation_window.cpp
+++ src/simulation_window.cpp
@@ -12,13 +12,13 @@ SimulationWindow::SimulationWindow()
 {
     window.create(sf::VideoMode(640, 480), "UniverseSim");
 
-    sf::Font* font = new sf::Font();
+    sf::Font *font = new sf::Font();
     initFont(font);
-    
-    Button* button = new Button(550, 10, 80, 50, "Add button", font, sf::Color::Blue, sf::Color::Cyan);
+
+    Button *button = new Button(550, 10, 80, 50, "Add button", font, sf::Color::Blue, sf::Color::Cyan);
     buttons.push_back(button);
 
-    TextPanel* textPanel = new TextPanel(sf::Vector2f(540, 80), sf::Vector2f(80, 30), "", font, 12, TextHorizontalAlign::LEFT);
+    TextPanel *textPanel = new TextPanel(sf::Vector2f(540, 80), sf::Vector2f(80, 30), "", font, 12, TextHorizontalAlign::LEFT);
     textPanels.push_back(textPanel);
     textPanel = new TextPanel(sf::Vector2f(540, 120), sf::Vector2f(80, 30), "", font, 12, TextHorizontalAlign::LEFT);
     textPanels.push_back(textPanel);
@@ -30,7 +30,7 @@ SimulationWindow::SimulationWindow()
 
     spaceArea.setPosition(0, 0);
     spaceArea.setSize(sf::Vector2f(window.getSize().x - widgetsPanelWidth, window.getSize().y));
-    
+
     state = DEFAULT;
 
     capturedBody = nullptr;
@@ -39,13 +39,13 @@ SimulationWindow::SimulationWindow()
 void SimulationWindow::runSimulation()
 {
     while (window.isOpen())
-    {        
+    {
         listenEvents();
         window.clear();
 
         update(sf::Mouse::getPosition(window));
         render();
-        
+
         window.display();
     }
 }
@@ -61,14 +61,14 @@ void SimulationWindow::listenEvents()
             window.close();
         if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
         {
-            if(state != ADD_PLANET)
+            if (state != ADD_PLANET)
                 releaseBody();
         }
 
-        if(event.type == sf::Event::MouseWheelScrolled)
+        if (event.type == sf::Event::MouseWheelScrolled)
         {
             // Change body mass if selected
-            if(capturedBody != nullptr)
+            if (capturedBody != nullptr)
             {
                 double capturedBodyMass = capturedBody->getMass();
                 float delta = event.mouseWheelScroll.delta;
@@ -76,15 +76,15 @@ void SimulationWindow::listenEvents()
             }
         }
 
-        if(event.type == sf::Event::MouseMoved)
+        if (event.type == sf::Event::MouseMoved)
         {
-            if(dragAndDrop.active)
+            if (dragAndDrop.active)
             {
                 dragAndDrop.direction = sf::Mouse::getPosition() - dragAndDrop.startPosition;
             }
         }
 
-        if(event.type == sf::Event::MouseButtonPressed)
+        if (event.type == sf::Event::MouseButtonPressed)
         {
             dragAndDrop.active = true;
             dragAndDrop.startPosition = sf::Mouse::getPosition();
@@ -92,79 +92,83 @@ void SimulationWindow::listenEvents()
 
             sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
 
-            if(event.mouseButton.button == sf::Mouse::Button::Left)
+            if (event.mouseButton.button == sf::Mouse::Button::Left)
             {
-                if(state == DEFAULT)
+                if (state == DEFAULT)
                 {
-                    if(buttons[0]->getRect().contains(sf::Mouse::getPosition(window)))
+                    if (buttons[0]->getRect().contains(sf::Mouse::getPosition(window)))
                     {
                         state = ADD_PLANET;
-                        Body* newPlanet = new Body(100, 20);
+                        Body *newPlanet = new Body(100, 20);
                         newPlanet->setState(NEW);
                         capturedBody = newPlanet;
                         simulation.addPlanet(newPlanet);
                     }
                     else
                     {
-                        for(Body* body: simulation.getPlanets())
-                            if(body->getShape()->getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+                        for (Body *body : simulation.getPlanets())
+                            if (body->getShape()->getGlobalBounds().contains(mousePosition.x, mousePosition.y))
                             {
                                 releaseBody();
                                 captureBody(body);
                                 break;
                             }
                     }
-                    
                 }
-                else if(state == ADD_PLANET)
-                {
-                    if(capturedBody == nullptr)
-                        throw std::runtime_error("capturedBody can't be NULL! at line number " + std::to_string(__LINE__));
-                    bool canAddBody = true;
-                    for(Body* body: simulation.getPlanets())
-                        if(body != capturedBody)
-                            if(capturedBody->isCollided(*body))
-                                canAddBody = false;
-                    
-                    if(!intersect(spaceArea, *capturedBody->getShape()))
-                        canAddBody = false;
-
-                    if(canAddBody)
-                    {
-                        releaseBody();
-                        state = DEFAULT;
-                    }
-                }
-                
             }
-            // else if(dragAndDrop.mouseButton == sf::Mouse::Left)
-            // {
-            //     Force force(sf::Vector2f(dragAndDrop.direction) * -0.1f);
-            //     selectedBody->force(&force);
-            // }
         }
 
-        if(event.type == sf::Event::MouseButtonReleased)
+        if (event.type == sf::Event::MouseButtonReleased)
         {
-            if(dragAndDrop.mouseButton == event.mouseButton.button)
+            sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+
+            if (dragAndDrop.mouseButton == event.mouseButton.button)
             {
                 dragAndDrop.active = false;
 
-                if(capturedBody != nullptr)
+                if (capturedBody != nullptr)
                 {
                     // Update body radius if mouse dragged with pressed right button
-                    if(dragAndDrop.mouseButton == sf::Mouse::Right)
+                    if (dragAndDrop.mouseButton == sf::Mouse::Right)
                     {
                         double bodyRadius = capturedBody->getRadius();
                         double multiplier = dragAndDrop.direction.y > 0 ? 0.1 : -0.1;
                         capturedBody->setRadius(std::max(10., bodyRadius + multiplier * norm(sf::Vector2f(dragAndDrop.direction))));
 
-                        for(Body* body: simulation.getPlanets())
-                            if(body->isCollided(*capturedBody) && capturedBody != body)
+                        for (Body *body : simulation.getPlanets())
+                            if (body->isCollided(*capturedBody) && capturedBody != body)
                             {
                                 capturedBody->setRadius(bodyRadius);
                                 break;
                             }
+                    }
+                    else if (dragAndDrop.mouseButton == sf::Mouse::Left)
+                    {
+                        if (spaceArea.getGlobalBounds().contains(sf::Vector2f(mousePosition)))
+                        {
+                            Force force(sf::Vector2f(dragAndDrop.direction) * -0.1f);
+                            capturedBody->force(&force);
+                        }
+
+                        if (state == ADD_PLANET)
+                        {
+                            if (capturedBody == nullptr)
+                                throw std::runtime_error("capturedBody can't be NULL! at line number " + std::to_string(__LINE__));
+                            bool canAddBody = true;
+                            for (Body *body : simulation.getPlanets())
+                                if (body != capturedBody)
+                                    if (capturedBody->isCollided(*body))
+                                        canAddBody = false;
+
+                            if (!intersect(spaceArea, *capturedBody->getShape()))
+                                canAddBody = false;
+
+                            if (canAddBody)
+                            {
+                                releaseBody();
+                                state = DEFAULT;
+                            }
+                        }
                     }
                 }
             }
@@ -174,31 +178,31 @@ void SimulationWindow::listenEvents()
 
 void SimulationWindow::update(sf::Vector2i mousePosition)
 {
-    std::vector<Body*>& planets = simulation.getPlanets(); 
-    if(state != ADD_PLANET && !dragAndDrop.active)
+    std::vector<Body *> &planets = simulation.getPlanets();
+    if (state != ADD_PLANET && !dragAndDrop.active)
         selectedBody = nullptr;
 
-    for(int i = 0; i < planets.size(); i++)
+    for (int i = 0; i < planets.size(); i++)
     {
         Force totalForce(sf::Vector2f(0, 0));
-        for(int j = 0; j < planets.size(); j++)
+        for (int j = 0; j < planets.size(); j++)
         {
-            if(i != j)
+            if (i != j)
             {
-                if(planets[i]->getState() == NEW || planets[j]->getState() == NEW)
+                if (planets[i]->getState() == NEW || planets[j]->getState() == NEW)
                     continue;
                 Force force = planets[i]->calcGravity(*planets[j]);
                 totalForce = totalForce + force;
 
-                if(planets[i]->isCollided(*planets[j]))
+                if (planets[i]->isCollided(*planets[j]))
                 {
                     simulation.processColision(*planets[i], *planets[j]);
                     break;
                 }
             }
         }
-      
-        if(planets[i]->getShape()->getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y))
+
+        if (planets[i]->getShape()->getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y))
         {
             selectedBody = planets[i];
         }
@@ -206,21 +210,22 @@ void SimulationWindow::update(sf::Vector2i mousePosition)
         planets[i]->move();
     }
 
-    for(Button* button: buttons)
+    for (Button *button : buttons)
         button->update(mousePosition);
 
-    if(selectedBody != nullptr)
+    if (selectedBody != nullptr)
     {
         textPanels[0]->update("Radius: " + std::to_string(int(selectedBody->getRadius())));
         textPanels[1]->update("Mass: " + std::to_string(int(selectedBody->getMass())));
     }
     else
-        for(TextPanel* textPanel: textPanels)
+        for (TextPanel *textPanel : textPanels)
             textPanel->update("");
 
-    if(capturedBody != nullptr)
-        if(capturedBody->getState() == NEW)
-            capturedBody->moveTo(sf::Vector2f(mousePosition));
+    if (capturedBody != nullptr)
+        if (capturedBody->getState() == NEW)
+            if(!dragAndDrop.active)
+                capturedBody->moveTo(sf::Vector2f(mousePosition));
 }
 
 void SimulationWindow::render()
@@ -228,39 +233,38 @@ void SimulationWindow::render()
     // window.draw(spaceArea);
     window.draw(widgetsPanel);
 
-    std::vector<Body*>& planets = simulation.getPlanets(); 
-    for(int i = 0; i < planets.size(); i++)
-        if(intersect(spaceArea, *planets[i]->getShape()))
+    std::vector<Body *> &planets = simulation.getPlanets();
+    for (int i = 0; i < planets.size(); i++)
+        if (intersect(spaceArea, *planets[i]->getShape()))
             planets[i]->render(&window);
 
-    for(Button* button: buttons)
+    for (Button *button : buttons)
         button->render(&window);
 
-    for(TextPanel* textPanel: textPanels)
-        textPanel->render(&window);    
+    for (TextPanel *textPanel : textPanels)
+        textPanel->render(&window);
 }
 
-
-void SimulationWindow::initFont(sf::Font* font)
+void SimulationWindow::initFont(sf::Font *font)
 {
-    if(!font->loadFromFile("./res/fonts/Ubuntu-B.ttf"))
+    if (!font->loadFromFile("./res/fonts/Ubuntu-B.ttf"))
     {
         std::cout << "Can't load fonts!" << std::endl;
     }
 }
 
-void printVector(const sf::Vector2f& vector)
+void printVector(const sf::Vector2f &vector)
 {
-    std::cout << vector.x << ' ' << vector.y << std::endl; 
+    std::cout << vector.x << ' ' << vector.y << std::endl;
 }
 
-bool SimulationWindow::intersect(const sf::RectangleShape& rect, const sf::CircleShape& circle)
+bool SimulationWindow::intersect(const sf::RectangleShape &rect, const sf::CircleShape &circle)
 {
-    return rect.getGlobalBounds().contains(circle.getPosition().x, circle.getPosition().y) && 
+    return rect.getGlobalBounds().contains(circle.getPosition().x, circle.getPosition().y) &&
            rect.getGlobalBounds().contains(circle.getPosition().x + 2 * circle.getRadius(), circle.getPosition().y + 2 * circle.getRadius());
 }
 
-void SimulationWindow::captureBody(Body* body)
+void SimulationWindow::captureBody(Body *body)
 {
     capturedBody = body;
     capturedBody->setState(CAPTURED);
@@ -268,7 +272,7 @@ void SimulationWindow::captureBody(Body* body)
 
 void SimulationWindow::releaseBody()
 {
-    if(capturedBody != nullptr)
+    if (capturedBody != nullptr)
     {
         capturedBody->setState(ACTIVE);
         capturedBody = nullptr;
